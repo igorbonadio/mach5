920e4b5c0dbf67ea9ec6520272d9f622d05c5d64
Igor Bonadio <igorbonadio@gmail.com>
2014-01-06 18:23:40 -0200

    Better generated names

--- sample/Sort.cpp
+++ sample/Sort.cpp
@@ -4,12 +4,12 @@
 
 #include "Mach5.hpp"
 
-class ArrayOfNumbers : public ::mach5::BenchmarkFixture {
+class SortingNumberWith : public ::mach5::BenchmarkFixture {
 public:
 	std::vector<int> num = {0, 9, 8, 7, 6, 5, 4, 3, 2, 1};
 };
 
-BENCHMARK_F(ArrayOfNumbers, SelectionSort, 100, 200) {
+BENCHMARK_F(SortingNumberWith, SelectionSort, 100, 200) {
 	int tam = num.size();
 	for (int i = 0; i < (tam-1); i++) {
 		int min = i;
@@ -26,7 +26,8 @@ BENCHMARK_F(ArrayOfNumbers, SelectionSort, 100, 200) {
 	}
 }
 
-BENCHMARK_F(ArrayOfNumbers, Sort, 100, 100) {
+BENCHMARK(DefaultCppSort, 100, 100) {
+	std::vector<int> num = {0, 9, 8, 7, 6, 5, 4, 3, 2, 1};
 	std::sort(num.begin(), num.end());
 }
 
--- src/Mach5.hpp
+++ src/Mach5.hpp
@@ -4,20 +4,38 @@
 #include "Benchmark.hpp"
 #include "BenchmarkRunner.hpp"
 
-#define _BENCHMARK_NAME(benchmark_name) \
-  benchmark_name ## Benchmark
+#define _BENCHMARK_F_NAME(benchmark_fixture, benchmark_name) \
+  benchmark_fixture ## benchmark_name ## Benchmark
 
-#define _BENCHMARK_FACTORY_NAME(benchmark_name) \
-  _ ## benchmark_name ## BenchmarkFactory
+#define _BENCHMARK_F_FACTORY_NAME(benchmark_fixture, benchmark_name) \
+  _ ## benchmark_fixture ## benchmark_name ## BenchmarkFactory
 
-#define _BENCHMARK_CLASS(benchmark_name) \
-  class _BENCHMARK_NAME(benchmark_name) : public Benchmark { \
+
+#define _BENCHMARK_F_CLASS(benchmark_fixture, benchmark_name) \
+  class _BENCHMARK_F_NAME(benchmark_fixture, benchmark_name) : public benchmark_fixture { \
   public: \
     virtual void code(); \
   };
 
-#define _BENCHMARK_F_CLASS(benchmark_fixture, benchmark_name) \
-  class _BENCHMARK_NAME(benchmark_name) : public benchmark_fixture { \
+#define _BENCHMARK_F_FACTORY(benchmark_fixture, benchmark_name, benchmark_runs, benchmark_iterations) \
+  const auto _BENCHMARK_F_FACTORY_NAME(benchmark_fixture, benchmark_name) = ::mach5::BenchmarkRunner::instance().addBenchmark<_BENCHMARK_F_NAME(benchmark_fixture, benchmark_name)>(#benchmark_fixture#benchmark_name, benchmark_runs, benchmark_iterations);
+
+#define _BENCHMARK_F_CODE(benchmark_fixture, benchmark_name) \
+  void _BENCHMARK_F_NAME(benchmark_fixture, benchmark_name)::code()
+
+#define BENCHMARK_F(benchmark_fixture, benchmark_name, benchmark_runs, benchmark_iterations) \
+  _BENCHMARK_F_CLASS(benchmark_fixture, benchmark_name) \
+  _BENCHMARK_F_FACTORY(benchmark_fixture, benchmark_name, benchmark_runs, benchmark_iterations) \
+  _BENCHMARK_F_CODE(benchmark_fixture, benchmark_name)
+
+#define _BENCHMARK_NAME(benchmark_name) \
+  benchmark_name ## Benchmark
+
+#define _BENCHMARK_FACTORY_NAME(benchmark_name) \
+  benchmark_name ## BenchmarkFactory
+
+#define _BENCHMARK_CLASS(benchmark_name) \
+  class _BENCHMARK_NAME(benchmark_name) : public ::mach5::Benchmark { \
   public: \
     virtual void code(); \
   };
@@ -30,11 +48,6 @@
 
 #define BENCHMARK(benchmark_name, benchmark_runs, benchmark_iterations) \
   _BENCHMARK_CLASS(benchmark_name) \
-  _BENCHMARK_FACTORY(benchmark_name, benchmark_runs, benchmark_iterations) \
-  _BENCHMARK_CODE(benchmark_name)
-
-#define BENCHMARK_F(benchmark_fixture, benchmark_name, benchmark_runs, benchmark_iterations) \
-  _BENCHMARK_F_CLASS(benchmark_fixture, benchmark_name) \
   _BENCHMARK_FACTORY(benchmark_name, benchmark_runs, benchmark_iterations) \
   _BENCHMARK_CODE(benchmark_name)
 
