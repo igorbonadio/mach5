835cdfa85f06376bde0cd65867d2b8a8c33c5539
Igor Bonadio <igorbonadio@gmail.com>
2014-01-13 14:23:09 -0200

    Adds SINGLE and FOR benchmark's parameters

--- src/Mach5.hpp
+++ src/Mach5.hpp
@@ -6,6 +6,12 @@
 #include "Benchmark.hpp"
 #include "BenchmarkRunner.hpp"
 
+#define FOR(var_name, start, end) \
+  var_name, start, end
+
+#define SINGLE \
+  segment, 0, 0
+
 // with fixture
 
 #define _BENCHMARK_F_NAME(benchmark_fixture, benchmark_name) \
@@ -18,19 +24,22 @@
 #define _BENCHMARK_F_CLASS(benchmark_fixture, benchmark_name) \
   class _BENCHMARK_F_NAME(benchmark_fixture, benchmark_name) : public benchmark_fixture { \
   public: \
-    virtual void code(); \
+    virtual void code(int segment); \
   };
 
-#define _BENCHMARK_F_FACTORY(benchmark_fixture, benchmark_name, benchmark_runs, benchmark_iterations) \
-  const auto _BENCHMARK_F_FACTORY_NAME(benchmark_fixture, benchmark_name) = ::mach5::BenchmarkRunner::instance().addBenchmark<_BENCHMARK_F_NAME(benchmark_fixture, benchmark_name)>(#benchmark_fixture#benchmark_name, benchmark_runs, benchmark_iterations);
+#define _BENCHMARK_F_FACTORY(benchmark_fixture, benchmark_name, benchmark_runs, benchmark_iterations, start, end) \
+  const auto _BENCHMARK_F_FACTORY_NAME(benchmark_fixture, benchmark_name) = ::mach5::BenchmarkRunner::instance().addBenchmark<_BENCHMARK_F_NAME(benchmark_fixture, benchmark_name)>(#benchmark_fixture#benchmark_name, benchmark_runs, benchmark_iterations, start, end);
 
-#define _BENCHMARK_F_CODE(benchmark_fixture, benchmark_name) \
-  void _BENCHMARK_F_NAME(benchmark_fixture, benchmark_name)::code()
+#define _BENCHMARK_F_CODE(benchmark_fixture, benchmark_name, var_name) \
+  void _BENCHMARK_F_NAME(benchmark_fixture, benchmark_name)::code(int var_name)
+
+#define _BENCHMARK_F_FACTORY_AND_CODE(var_name, start, end, benchmark_fixture, benchmark_name, benchmark_runs, benchmark_iterations) \
+  _BENCHMARK_F_FACTORY(benchmark_fixture, benchmark_name, benchmark_runs, benchmark_iterations, start, end) \
+  _BENCHMARK_F_CODE(benchmark_fixture, benchmark_name, var_name)
 
 #define BENCHMARK_F(benchmark_fixture, benchmark_name, benchmark_runs, benchmark_iterations, type) \
   _BENCHMARK_F_CLASS(benchmark_fixture, benchmark_name) \
-  _BENCHMARK_F_FACTORY(benchmark_fixture, benchmark_name, benchmark_runs, benchmark_iterations) \
-  _BENCHMARK_F_CODE(benchmark_fixture, benchmark_name)
+  _BENCHMARK_F_FACTORY_AND_CODE(type, benchmark_fixture, benchmark_name, benchmark_runs, benchmark_iterations)
 
 // without fixture
 
@@ -43,19 +52,22 @@
 #define _BENCHMARK_CLASS(benchmark_name) \
   class _BENCHMARK_NAME(benchmark_name) : public ::mach5::Benchmark { \
   public: \
-    virtual void code(); \
+    virtual void code(int segment); \
   };
 
-#define _BENCHMARK_FACTORY(benchmark_name, benchmark_runs, benchmark_iterations) \
-  const auto _BENCHMARK_FACTORY_NAME(benchmark_name) = ::mach5::BenchmarkRunner::instance().addBenchmark<_BENCHMARK_NAME(benchmark_name)>(#benchmark_name, benchmark_runs, benchmark_iterations);
+#define _BENCHMARK_FACTORY(benchmark_name, benchmark_runs, benchmark_iterations, start, end) \
+  const auto _BENCHMARK_FACTORY_NAME(benchmark_name) = ::mach5::BenchmarkRunner::instance().addBenchmark<_BENCHMARK_NAME(benchmark_name)>(#benchmark_name, benchmark_runs, benchmark_iterations, start, end);
 
-#define _BENCHMARK_CODE(benchmark_name) \
-  void _BENCHMARK_NAME(benchmark_name)::code()
+#define _BENCHMARK_CODE(benchmark_name, var_name) \
+  void _BENCHMARK_NAME(benchmark_name)::code(int var_name)
+
+#define _BENCHMARK_FACTORY_AND_CODE(var_name, start, end, benchmark_name, benchmark_runs, benchmark_iterations) \
+  _BENCHMARK_FACTORY(benchmark_name, benchmark_runs, benchmark_iterations, start, end) \
+  _BENCHMARK_CODE(benchmark_name, var_name)
 
 #define BENCHMARK(benchmark_name, benchmark_runs, benchmark_iterations, type) \
   _BENCHMARK_CLASS(benchmark_name) \
-  _BENCHMARK_FACTORY(benchmark_name, benchmark_runs, benchmark_iterations) \
-  _BENCHMARK_CODE(benchmark_name)
+  _BENCHMARK_FACTORY_AND_CODE(type, benchmark_name, benchmark_runs, benchmark_iterations)
 
 // main
 
