9e5bf3deb5e7b3cb45799ef6e0707cd529a4b292
Igor Bonadio <igorbonadio@gmail.com>
2014-02-01 13:08:49 -0200

    Add tests without macros

--- test/Sort.cpp
+++ test/Sort.cpp
@@ -17,24 +17,40 @@ public:
 	};
 };
 
-BENCHMARK_F(Fixture1, Single, 10, 2, SINGLE) {
-	std::sort(num.begin(), num.end());
-}
+class Fixture1Single : public Fixture1 {
+public:
+  virtual void code(int index) {
+  	std::sort(num.begin(), num.end());
+  }
+};
+const auto FactoryFixture1Single = ::mach5::BenchmarkRunner::instance().addBenchmark<Fixture1Single>("Fixture1Single", 10, 2, 0, 0);
 
-BENCHMARK_F(Fixture2, For, 10, 2, FOR(i, 0, 2)) {
-	std::sort(num[i].begin(), num[i].end());
-}
+class Fixture2For : public Fixture2 {
+public:
+  virtual void code(int index) {
+  	std::sort(num[index].begin(), num[index].end());
+  }
+};
+const auto FactoryFixture2For = ::mach5::BenchmarkRunner::instance().addBenchmark<Fixture2For>("Fixture2For", 10, 2, 0, 2);
 
-BENCHMARK(Single, 10, 2, SINGLE) {
-	std::vector<int> num = {0, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-	std::sort(num.begin(), num.end());
-}
+class Single : public ::mach5::Benchmark {
+public:
+  virtual void code(int index) {
+  	std::vector<int> num = {0, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+		std::sort(num.begin(), num.end());
+  }
+};
+const auto FactorySingle = ::mach5::BenchmarkRunner::instance().addBenchmark<Single>("Single", 10, 2, 0, 0);
 
-BENCHMARK(For, 10, 2, FOR(i, 0, 2)) {
-	std::vector<std::vector<int>> num = {
-		{0, 9, 8, 7, 6},
-		{0, 9, 8, 7, 6, 5, 4},
-		{0, 9, 8, 7, 6, 5, 4, 3, 2, 1}
-	};
-	std::sort(num[i].begin(), num[i].end());
-}
+class For : public ::mach5::Benchmark {
+public:
+  virtual void code(int index) {
+  	std::vector<std::vector<int>> num = {
+			{0, 9, 8, 7, 6},
+			{0, 9, 8, 7, 6, 5, 4},
+			{0, 9, 8, 7, 6, 5, 4, 3, 2, 1}
+		};
+		std::sort(num[index].begin(), num[index].end());
+  }
+};
+const auto FactoryFor = ::mach5::BenchmarkRunner::instance().addBenchmark<For>("For", 10, 2, 0, 2);
